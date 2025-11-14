7d45d7694555868de90a650051e1042de7b12289
Freddy Fuentes <24freddy.fuentes@glimnet.se>
2025-11-14 14:50:40 +0100

    skapat mappar för backend ex config,models,controllers etc, skapat database.js under config import MondoDB client

---
+++ backend/jest.config.js
@@ -0,0 +1,19 @@
+export default {
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
+    collectCoverageFrom: [
+      '**/*.js',
+      '!**/node_modules/**',
+      '!**/jest.config.js',
+    ],
+    coverageDirectory: 'coverage',
+    verbose: true,
+    transform: {},
+    extensionsToTreatAsEsm: ['.js'],
+    globals: {
+      'ts-jest': {
+        useESM: true,
+      },
+    },
+  };
+
